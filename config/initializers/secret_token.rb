cb5312ba4c44feef82293ee111ad5f2d18a7e30c
Arvind Soni <arvindsoni@gmail.com>
2013-02-12 22:53:16 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grubly::Application.config.secret_token = '745ea6f2fbd9442752a2d092d3b2568e131312100978090786904106c14d052c48ad8770742f8117c733f41d20bafddf40e5e9836e68203bebbf9c514028f1ee'
